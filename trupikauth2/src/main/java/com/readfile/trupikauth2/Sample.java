02520e148cbba0b7bd837f1cbada974be53ffa45
nagarjuna <nagarjuna90it@gmail.com>
2019-06-27 17:54:10 +0530

    new class added for library test

---
+++ trupikauth2/src/main/java/com/readfile/trupikauth2/Sample.java
@@ -0,0 +1,12 @@
+package com.readfile.trupikauth2;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Sample {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
