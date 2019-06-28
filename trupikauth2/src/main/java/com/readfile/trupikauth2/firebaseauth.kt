d0eb5ff1f1666d78cbc546e2b52242d441f1d193
nagarjuna <nagarjuna90it@gmail.com>
2019-06-28 13:06:09 +0530

    added classes

---
+++ trupikauth2/src/main/java/com/readfile/trupikauth2/firebaseauth.kt
@@ -0,0 +1,68 @@
+package com.readfile.trupikauth2
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.firebaseauth.*
+import java.util.*
+
+class firebaseauth : AppCompatActivity() {
+    lateinit var providers : List<AuthUI.IdpConfig>
+    val MY_REQUEST_CODE:Int = 7117
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.firebaseauth)
+
+        //init
+        providers = Arrays.asList<AuthUI.IdpConfig>(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+//            AuthUI.IdpConfig.FacebookBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build()
+        )
+        showSignINOptions()
+        btn_sign_out.setOnClickListener{
+            AuthUI.getInstance().signOut(this@firebaseauth)
+                .addOnCompleteListener {
+                    btn_sign_out.isEnabled=false
+                    showSignINOptions()
+                }
+                .addOnFailureListener {
+                        e->
+                    Toast.makeText(this@firebaseauth,e.message, Toast.LENGTH_LONG).show()
+                }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==MY_REQUEST_CODE){
+            val response = IdpResponse.fromResultIntent(data)
+            if(resultCode== Activity.RESULT_OK){
+                val user = FirebaseAuth.getInstance().currentUser
+                Toast.makeText(this,""+user!!.email, Toast.LENGTH_LONG).show()
+                btn_sign_out.isEnabled=true
+            }
+            else{
+                Toast.makeText(this,""+response!!.error!!.message, Toast.LENGTH_LONG).show()
+
+            }
+        }
+    }
+    public fun showSignINOptions(){
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+
+            .setAvailableProviders(providers)
+            .setTheme(R.style.MyTheme)
+            .build(),MY_REQUEST_CODE
+        )
+
+    }
+}
